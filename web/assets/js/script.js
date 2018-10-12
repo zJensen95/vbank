3ec70fbe426665cb4f3d1d8ebf05ea1e2c02a46d
zJensen95 <zac@nomineedesign.com>
2018-10-12 15:35:54 -0500

    Responsiveness on homepage and account page and menu functionality

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -1,7 +1,24 @@
+function addRemoveActive($this) {
+	if ($this.classList.contains('is-active')) {
+		$this.classList.remove('is-active');
+	} else {
+		$this.classList.add('is-active');
+	}
+}
 
-$(document).ready(function() {
-	$('.posts').masonry({
-	  columnWidth: '.sizing',
-	  itemSelector: '.post'
-	});
-})
+function openMenu() {
+	var menu = document.querySelector('menu');
+	
+	if ( menu.classList.contains('is-open') ) {
+		menu.classList.remove('is-open');
+	} else {
+		menu.classList.add('is-open');
+	}
+}
+
+document.getElementById('hamburger').addEventListener('click', function() {
+	
+	console.log('test');
+	openMenu();
+	
+});
